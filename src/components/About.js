34fdadb92edb0ea42bc474d9c729049ded2a5029
Ellen Sun <heeycutie@yahoo.com>
2018-09-20 23:58:10 -0400

    ADD link to bootstrap to index.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default class About extends Component {
     return (
       <Container className="about">
         <Row>
-          <Col sm={{ size: 12 }} md={{ size: 8, offset: 2 }}>
+          <Col sm={{ size: 10, offset: 1 }} md={{ size: 8, offset: 2 }}>
               <h2>About Me</h2>
               <p>While working as Product Manager for Kingsoft in Beijing, China, I was entranced by the developers within my team for their ability to create beautiful, user-friendly applications. After sampling some online courses with Codecademy and Code School, I fell in love and decided to change my career path, actively pursuing web development full-time. With my ambition, perseverance, and dedication, I have become an expert in front-end development; experienced in Ruby and Ruby on Rails; and proficient with JavaScript, jQuery, PHP, MySQL and the LAMP stack in MVC frameworks.</p>
               <p>Fully immersing myself in the Atlanta tech community, I regularly attend Meetups to learn new technologies and connect with others with similar passions. I have been able to use what I have learned to help other self-taught developers succeed and regularly lead the Atlanta group of FreeCodeCampers, an online full-stack bootcamp.</p>
